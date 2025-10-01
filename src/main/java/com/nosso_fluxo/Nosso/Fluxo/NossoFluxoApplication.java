aabfc911e872f346dce2d3ecd86fa26cda77e416
Thiago Tonelli da Silva <thiagktonelli@gmail.com>
2025-10-01 14:41:15 -0300

    primeiro commit

---
+++ src/main/java/com/nosso_fluxo/Nosso/Fluxo/NossoFluxoApplication.java
@@ -0,0 +1,13 @@
+package com.nosso_fluxo.Nosso.Fluxo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NossoFluxoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NossoFluxoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nosso_fluxo/Nosso/Fluxo/NossoFluxoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nosso_fluxo.Nosso.Fluxo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NossoFluxoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
